75990be8eea502e881adfbbf4a07ef6813979722
mehmet arif esen <mehmetarif96@gmail.com>
2023-01-27 16:31:02 +0300

    first commit

---
+++ src/lesson001/Deneme.java
@@ -0,0 +1,14 @@
+package lesson001;
+
+public class Deneme {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Deneme");
+
+		//ctrl + F11 hızlı çalıştırma
+		//BilgeAdamAlgoritma Reo oluşturalım
+		//bu projeyi githuba atalım
+	}
+
+}
